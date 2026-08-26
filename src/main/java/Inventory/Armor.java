559e228893a9daf375996460a7b6277fd62495cc
shehbaz <mdshehbaz666666@gmail.com>
2026-08-26 11:55:38 +0530

    - Define initial classes for Combat, Dungeon, Enemy, Inventory, and Player packages

---
+++ src/main/java/Inventory/Armor.java
@@ -0,0 +1,9 @@
+package PowerFactory;
+
+public class Armor {
+//    int "Soft_Ballistic_Armor" = 300;
+//    int "Hard_Plate_Armor" = 500;
+//    int "Gambeson" = 700;
+//    int "Leather Armor" = 300;
+
+}
---
+++ src/main/java/Inventory/item.java
@@ -0,0 +1,4 @@
+package PowerFactory;
+
+public class item {
+}
